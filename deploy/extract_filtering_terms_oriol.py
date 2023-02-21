d288d735acd48843afb6448b8642d6feba7b2435
costero-e <orildsaga@gmail.com>
2023-02-21 10:34:59 +0100

    cors changes

--- deploy/extract_filtering_terms_oriol.py
+++ deploy/extract_filtering_terms_oriol.py
@@ -12,6 +12,10 @@ from bson.objectid import ObjectId
 from owlready2 import OwlReadyOntologyParsingError
 from tqdm import tqdm
 import obonet
+from bson.json_util import dumps
+import json
+import networkx
+import os
 
 ONTOLOGY_REGEX = re.compile(r"([_A-Za-z]+):([_A-Za-z0-9^\-]+)")
 
@@ -33,7 +37,6 @@ class MyProgressBar:
             self.pbar.update(downloaded)
         else:
             self.pbar.finish()
-
 
 def insert_all_ontology_terms_used():
     collections = client.beacon.list_collection_names()
@@ -60,12 +63,12 @@ def get_ontology_name(ontology: owlready2.Ontology) -> str:
 
 def load_ontology(ontology_id: str) -> Optional[owlready2.Ontology]:
     if ontology_id.isalpha():
+        url_alt = "https://www.ebi.ac.uk/efo/EFO.obo"
         url = "http://purl.obolibrary.org/obo/{}.obo".format(ontology_id.lower())
         path = "ontologies/{}.obo".format(ontology_id)
         try:
             if not os.path.exists(path):
                 urllib.request.urlretrieve(url, path, MyProgressBar())
-            return '{}'.format(ontology_id)
         except HTTPError:
             # TODO: Handle error
             print("ERROR", HTTPError)
@@ -73,6 +76,18 @@ def load_ontology(ontology_id: str) -> Optional[owlready2.Ontology]:
         except ValueError:
             print("ERROR", ValueError)
             pass
+        print (os.stat(path).st_size)
+        if os.stat(path).st_size == 0:
+            try:
+                urllib.request.urlretrieve(url_alt, path, MyProgressBar())
+            except HTTPError:
+                # TODO: Handle error
+                print("ERROR", HTTPError)
+                pass
+            except ValueError:
+                print("ERROR", ValueError)
+                pass
+    return '{}'.format(ontology_id)
 
 
 def get_ontology_term_label(ontology: owlready2.Ontology, term: str) -> Optional[str]:
@@ -95,13 +110,77 @@ def get_ontology_term_count(collection_name: str, term: str) -> int:
         .get_collection(collection_name)\
         .count_documents(query)
 
+def get_ontology_field_name(ontology_id:str, term_id:str, collection:str):
+    query = {
+        '$text': {
+            '$search': '\"' + ontology_id + ":" + term_id + '\"'
+        }
+    }
+    results = client.beacon.get_collection(collection).find(query)
+    results = list(results)
+    results = dumps(results)
+    results = json.loads(results)
+    field = ''
+    for result in results:
+        for k, v in result.items():
+            if isinstance(v, str): 
+                if v == ontology_id + ':' + term_id:
+                    field = k
+                    break
+            elif isinstance(v, dict):
+                for k2, v2 in v.items():
+                    if v2 == ontology_id + ':' + term_id:
+                        field = k + '.' + k2
+                        break 
+            elif isinstance(v, list):
+                for item in v:
+                    print(item)
+                    if isinstance(item, str): 
+                        if item == ontology_id + ':' + term_id:
+                            field = item
+                            break
+                    elif isinstance(item, dict):
+                        for k2, v2 in item.items():
+                            if isinstance(v2, str):
+                                if v2 == ontology_id + ':' + term_id:
+                                    field = k2
+                                    break 
+                            elif isinstance(v2, dict):
+                                for k3, v3 in v2.items():
+                                    if isinstance(v3, str):
+                                        if v3 == ontology_id + ':' + term_id:
+                                            field = k2 + '.' + k3
+                                            break 
+                                    elif isinstance(v3, dict):
+                                        for k4, v4 in v3.items():
+                                            if v4 == ontology_id + ':' + term_id:
+                                                field = k2 + '.' + k3 + '.' + k4
+                                                break
+
+        print(field)
+    return field
+
+def get_descendants(ontology_id:str, ontology_term:str):        
+    url = 'ontologies/{}.obo'.format(ontology_id.upper())
+    graph = obonet.read_obo(url)
+    ontology = ontology_id + ':' + ontology_term
+    networkx.is_directed_acyclic_graph(graph)
+    try:
+        descendants = networkx.ancestors(graph, ontology)
+    except Exception:
+        descendants = ''
+    if not descendants:
+        descendants = {ontology}
+    descendants = list(descendants)
+    print(descendants)
+    return descendants
 
 def find_ontology_terms_used(collection_name: str) -> List[Dict]:
     terms = []
     terms_ids = set()
     ontologies = dict()
     count = client.beacon.get_collection(collection_name).estimated_document_count()
-    xs = client.beacon.get_collection(collection_name).find()
+    xs = client.beacon.get_collection(collection_name).find(no_cursor_timeout=True)
     for r in tqdm(xs, total=count):
         matches = ONTOLOGY_REGEX.findall(str(r))
         for ontology_id, term_id in matches:
@@ -119,7 +198,10 @@ def find_ontology_terms_used(collection_name: str) -> List[Dict]:
                         # TODO: Use conf.py -> beaconGranularity to not disclouse counts in the filtering terms
                         'count': get_ontology_term_count(collection_name, term),
                         'collection': collection_name,
+                        'field': get_ontology_field_name(ontology_id, term_id, collection_name),
+                        'descendants': get_descendants(ontology_id, term_id)
                     })
+    xs.close()
     return terms
 
 
--- deploy/obo.py
+++ deploy/obo.py
@@ -1,11 +1,13 @@
 import networkx
 import obonet
 
-url = '/Users/oriol/Desktop/beacon_canada/deploy/ontologies/ncit.obo'
+url = '/Users/oriol/Desktop/beacon_canada/deploy/ontologies/EFO.obo'
 graph = obonet.read_obo(url)
 
 # Number of nodes
-#len(graph)
+len(graph)
+
+print(len(graph))
 
 #print(graph.graph)
 '''
@@ -22,19 +24,22 @@ if '.' in name[0]:
 print(name)
 '''
 # Number of edges
-#graph.number_of_edges()
+graph.number_of_edges()
 
-#networkx.is_directed_acyclic_graph(graph)
+networkx.is_directed_acyclic_graph(graph)
 
 # Mapping from term ID to name
 
-id_to_name = {id_: data.get('name') for id_, data in graph.nodes(data=True)}
-print(id_to_name['NCIT:C173381'])
+#id_to_name = {id_: data.get('name') for id_, data in graph.nodes(data=True)}
+#print(id_to_name['NCIT:C173381'])
 #for id_, data in graph.nodes(data=True):
     #print (id_)
     #print(data.get('name'))
 #print(id_to_name['NCIT:C143048'] )
 
-#descendants = networkx.ancestors(graph, 'NCIT:C173381')
+descendants = networkx.ancestors(graph, 'EFO:0004600')
 
-#print(descendants)
+if not descendants:
+    descendants = {''}
+
+print(descendants)
