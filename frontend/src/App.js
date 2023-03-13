64ba4bfc1fa86c6ee148da908798c7509950199e
Gemma <johndoe@example.com>
2023-03-13 13:09:43 +0100

    frontendGemma

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,29 +29,55 @@ function Layout() {
   const [query, setQuery] = useState(null)
   const [exampleQ, setExampleQ] = useState([])
   const [showAdvSearch, setAdvSearch] = useState(false)
+  const [showAlphanumValue, setAlphanumValue] = useState(false)
   const [resultSetType, setResultsetType] = useState(["Select", "HIT", "MISS", "NONE", "ALL"])
   const [resultSet, setResultset] = useState("HIT")
+
   const [limit, setLimit] = useState(10)
-  const [skip, setSkip] = useState (0)
+  const [skip, setSkip] = useState(0)
+
+
+  const [ID, setId] = useState("")
+  const [operator, setOperator] = useState("")
+  const [value, setValue] = useState("")
+
+
+  const [descendantTermType, setDescendantTermType ] = useState(["true", "false"])
+  const [descendantTerm, setDescendantTerm] = useState("true")
+
+  const [similarityType, setSimilarityType] = useState(["Select", "low", "medium", "high"])
+  const [similarity, setSimilarity] = useState("Select")
 
   const Add = collectionType.map(Add => Add)
 
   const Add2 = resultSetType.map(Add2 => Add2)
 
+  const Add3 = descendantTermType.map(Add3 => Add3)
+
+  const Add4 = similarityType.map(Add4 => Add4)
+
   const handleAddrTypeChange = (e) => {
-   
+
     setCollection(collectionType[e.target.value])
     setExampleQ([])
 
   }
 
-  const handleClick = (e) =>{
+  const handleClick = (e) => {
     setCollectionType(["Select", "Individuals", "Cohorts", "Datasets", "Biosamples", "Analyses", "Runs", "Variant"])
     setCollection(collectionType[e.target.value])
   }
 
   const handleResultsetChanges = (e) => {
     setResultset(resultSetType[e.target.value])
+  }
+
+  const handleDescendantTermChanges = (e) => {
+    setDescendantTerm(descendantTermType[e.target.value])
+  }
+
+  const handleSimilarityChanges = (e) => {
+    setSimilarity(similarityType[e.target.value])
   }
 
   const handleSkipChanges = (e) => {
@@ -66,8 +92,26 @@ function Layout() {
     setAdvSearch(true)
   }
 
+
+  const handleIdChanges = (e) => {
+    setId(e.target.value)
+  }
+
+  const handleOperatorChanges = (e) => {
+    setOperator(e.target.value)
+  }
+
+  const handleValueChanges = (e) => {
+    setValue(e.target.vlue)
+  }
+
+  const handleAlphanumSearch = (e) => {
+    setAlphanumValue(true)
+  }
+
   const handleBasicSearch = (e) => {
     setAdvSearch(false)
+    setAlphanumValue(false)
   }
 
   const handleExQueries = () => {
@@ -102,18 +146,18 @@ function Layout() {
 
 
   const onSubmit = async (event) => {
-   
+
     event.preventDefault()
 
     setCollectionType(["Select"])
-   
-    
+
+
     setExampleQ([])
-    
-    
-   
+
+
+
     setAdvSearch(false)
-    
+    setAlphanumValue(false)
 
     try {
       if (query === '1' || query === '') {
@@ -135,7 +179,7 @@ function Layout() {
   function search(e) {
     setQuery(e.target.value)
     setResults(null)
-  
+
   }
 
   return (
@@ -145,7 +189,7 @@ function Layout() {
       </a>
       <nav className="navbar">
         <div className="container-fluid">
-          <select className="form-select" aria-label="Default select example" onClick = {handleClick} onChange={e => { handleAddrTypeChange(e) }}>
+          <select className="form-select" aria-label="Default select example" onClick={handleClick} onChange={e => { handleAddrTypeChange(e) }}>
             {
               Add.map((collection, key) => <option key={key} value={key}>{collection}
               </option>)
@@ -156,54 +200,110 @@ function Layout() {
             {!showAdvSearch && <button className="searchButton" type="submit"><img className="searchIcon" src="./magnifier.png" alt='searchIcon'></img></button>}
           </form>
         </div>
-        {!showAdvSearch && <button className="advSearch" onClick={handleAdvancedSearch}>
-          Advanced search
-        </button>}
-        {showAdvSearch && <form className='advSearchForm' onSubmit={onSubmit}>
-          <div className='advSearchModule'>
-            <label>SKIP</label>
-            <input className="skipForm" type="number" autoComplete='on' placeholder={0} onChange={(e) => handleSkipChanges(e)} aria-label="Skip" />
-            <label>LIMIT</label>
-            <input className="limitForm" type="number" autoComplete='on' placeholder={10} onChange={(e) => handleLimitChanges(e)} aria-label="Limit" />
-          </div>
 
-          <div className='advSearchModule'>
-            <label>Include Resultset Responses</label>
-            <select className="form-select2" aria-label="" onChange={e => { handleResultsetChanges(e) }}>
-              {
-                Add2.map((resultSet, key) => <option key={key} value={key}>{resultSet}
-                </option>)
-              }
-            </select>
+        <div className="additionalOptions">
+
+          {!showAdvSearch && <button className="advSearch" onClick={handleAdvancedSearch}>
+            Advanced search
+          </button>}
+          <div className="example">
+            <button className="exampleQueries" onClick={handleExQueries}>Query Examples</button>
+            <img className="bulbLogo" src="../light-bulb.png" alt='bulbIcon'></img>
+            <div>
+              {exampleQ[0] && exampleQ.map((result) => {
+
+                return (<div id='exampleQueries'>
+
+                  <button className="exampleQuery" onClick={() => { setPlaceholder(`${result}`); setQuery(`${result}`); setResults(null) }} >{result}</button>
+                </div>)
+
+              })}
+            </div>
+
           </div>
-          <div className='advSearchModule'>
-            <label>Similarity</label>
-            <input className="limitForm" type="text" autoComplete='on' placeholder={""} onChange={(e) => search(e)} aria-label="Search" />
-            <label>Include Descendant Terms</label>
-            <input className="limitForm" type="text" autoComplete='on' placeholder={""} onChange={(e) => search(e)} aria-label="Search" />
-          </div>
-          <button className="searchButton2" type="submit"><img className="searchIcon" src="./magnifier.png" alt='searchIcon'></img></button>
-        </form>}
+          {!showAlphanumValue && <button className="advSearch" onClick={handleAlphanumSearch}>
+            Alphanumerical Value Query
+          </button>}
+
+
+        </div>
+
+        <form className='advSearchForm' onSubmit={onSubmit}>
+
+          {showAdvSearch && <div>
+
+            <hr></hr>
+            <div className='advSearchModule'>
+              <div>
+                <label>SKIP</label>
+                <input className="skipForm" type="number" autoComplete='on' placeholder={0} onChange={(e) => handleSkipChanges(e)} aria-label="Skip" />
+              </div>
+              <div>
+                <label>LIMIT</label>
+                <input className="limitForm" type="number" autoComplete='on' placeholder={10} onChange={(e) => handleLimitChanges(e)} aria-label="Limit" />
+              </div>
+              <div className='resultset'>
+                <label>Include Resultset Responses</label>
+                <select className="form-select2" aria-label="" onChange={e => { handleResultsetChanges(e) }}>
+                  {
+                    Add2.map((resultSet, key) => <option key={key} value={key}>{resultSet}
+                    </option>)
+                  }
+                </select>
+              </div>
+
+            </div>
+            <div className='advSearchModule2'>
+              <div> 
+              
+                <label>Similarity</label>
+                <select className="form-select2" aria-label="" onChange={e => { handleSimilarityChanges(e) }}>
+                  {
+                    Add4.map((similarity, key) => <option key={key} value={key}>{similarity}
+                    </option>)
+                  }
+                </select>
+              </div>
+              <div>
+                <label>Include Descendant Terms</label>
+                <select className="form-select2" aria-label="" onChange={e => { handleDescendantTermChanges(e) }}>
+                  {
+                    Add3.map((descendantTerm, key) => <option key={key} value={key}>{descendantTerm}
+                    </option>)
+                  }
+                </select>
+               
+              </div>
+
+            </div>
+          </div>}
+
+
+          {showAlphanumValue && <div className='alphanumContainer'>
+            <hr></hr>
+            <div className='alphanumContainer2'>
+              <label>ID</label>
+              <input className="IdForm" type="text" autoComplete='on' placeholder={"write the ID"} onChange={(e) => handleIdChanges(e)} aria-label="ID" />
+              <label>Operator</label>
+              <input className="OperatorForm" type="text" autoComplete='on' placeholder={"="} onChange={(e) => handleOperatorChanges(e)} aria-label="Operator" />
+              <label>Value</label>
+              <input className="ValueForm" type="text" autoComplete='on' placeholder={"free text/ value"} onChange={(e) => handleValueChanges(e)} aria-label="Value" />
+            </div>
+
+          </div>}
+          {(showAlphanumValue || showAdvSearch) && <button className="searchButton" type="submit"><img className="searchIcon" src="./magnifier.png" alt='searchIcon'></img></button>}
+        </form>
+
+
 
       </nav>
-      <div className="example">
-        <button className="exampleQueries" onClick={handleExQueries}>Query Examples</button>
-        <img className="bulbLogo" src="../light-bulb.png" alt='bulbIcon'></img>
-        {exampleQ[0] && exampleQ.map((result) => {
-          return (<div id='exampleQueries'>
 
-            <button className="exampleQuery" onClick={() => { setPlaceholder(`${result}`); setQuery(`${result}`); setResults(null) }} >{result}</button>
-          </div>)
-
-        })}
-
-      </div>
-      {showAdvSearch && <button className='returnBasic' onClick={handleBasicSearch}>RETURN TO BASIC SEARCH</button>}
+      {(showAlphanumValue || showAdvSearch) && <button className='returnBasic' onClick={handleBasicSearch}>RETURN TO BASIC SEARCH</button>}
 
       <hr></hr>
       <div className="results">
         {results === null && <ResultsDatasets />}
-        {results === 'Individuals' && <Individuals2 query={query} resultSets={resultSet} limit={limit} skip={skip}/>}
+        {results === 'Individuals' && <Individuals2 query={query} resultSets={resultSet} limit={limit} skip={skip} ID={ID} operator={operator} value={value} descendantTerm={descendantTerm} similarity={similarity}/>}
       </div>
     </div>
 
